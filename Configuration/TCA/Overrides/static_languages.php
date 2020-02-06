29ec8e44f351a39da078ce2280502a719b038907
Florian Wessels <f.wessels@bitmotion.de>
2020-02-06 19:13:30 +0100

    [FEATURE] Introduce TYPO3 v10.2 compatibility

--- Configuration/TCA/Overrides/static_languages.php
+++ Configuration/TCA/Overrides/static_languages.php
@@ -2,12 +2,10 @@
 declare(strict_types = 1);
 defined('TYPO3_MODE') || die;
 
-$initialize = function ($dataSetName) {
-    $additionalFields = [
-        'lg_name_en' => 'lg_name_ar',
-    ];
-
-    \Bitmotion\StaticInfoTablesAr\Provider\TcaProvider::generateAndRegisterTca($additionalFields, $dataSetName);
-};
-$initialize('static_languages');
-unset($initialize);
+call_user_func(
+    function ($additionalFields, $dataSetName) {
+        \Bitmotion\StaticInfoTablesAr\Provider\TcaProvider::generateAndRegisterTca($additionalFields, $dataSetName);
+    },
+    ['lg_name_en' => 'lg_name_ar'],
+    'static_languages'
+);
